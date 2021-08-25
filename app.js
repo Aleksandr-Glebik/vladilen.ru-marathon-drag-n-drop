c73d10e59fbbeee9dd6a680d9b81f48e1489d0a6
Aleksandr-Glebik <glebikaa@mail.ru>
2021-08-25 23:03:45 +0300

    feat: cycle for of for placeholer and func dragstart /-end /-over/-enter/-leave/-drop

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const item = document.querySelector('.item')
 
+const placeholders = document.querySelectorAll('.placeholder')
 // console.log(item)
 
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    // console.log(placeholder)
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+
+}
 
 function dragstart(event) {
     // console.log('drag start', event.target)
@@ -16,5 +26,25 @@ function dragend() {
     // console.log('drag end')
     // event.target.classList.remove('hold', 'hide')
     event.target.className = 'item'
+}
 
+function dragover(event) {
+    event.preventDefault()
+    // console.log('drag over')
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+    // console.log('drag enter')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+    // console.log('drag leave')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    // console.log('drag drop')
+    event.target.append(item)
 }
